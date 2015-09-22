1979e549967da12b1a47f02b0fbceb9b000d9b0b
Bruce Merry <bmerry@ska.ac.za>
2015-09-22 16:00:27 +0200

    Make setup work on any Python version

--- code/bpy/setup.py
+++ code/bpy/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, Extension
+import sys
 
 extension = Extension(
     'bpy_petshop',
@@ -7,6 +8,6 @@ extension = Extension(
     depends=['../cpp/petshop.h'],
     language='c++',
     include_dirs=['../cpp'],
-    libraries=['boost_python-py34'])
+    libraries=['boost_python-py{}{}'.format(sys.version_info.major, sys.version_info.minor)])
 
 setup(name='bpy_petshop', ext_modules=[extension])
